723106caf1151d19212af9163704867466310209
Mustafa Khan <diysplat@gmail.com>
2022-07-18 09:27:18 -0400

    Created frame of test function
    Created the layout of the test function. Still need to implement the expected output values and the input arrays for frq and fst.

--- tests/test_megwas.py
+++ tests/test_megwas.py
@@ -1,6 +1,12 @@
 from unittest import TestCase
-from SimRA.statistical_genetics import _megwas
+from ..SimRA.statistical_genetics import _genotypes
 
-def test_is_equal():
-    assert (_megwas.is_equal(2,2))
-    assert (not _megwas.is_equal(2,1))
+print(_genotypes.model_bn)
+
+
+class Test_model_bn(TestCase):
+    
+    def test_model_bn(self):
+        result_pop_matrix, result_pop_idx = _genotypes.model_bn(3, 1000000, 1000, frq, fst, 3) # frq and fst are placeholders
+        self.assertEqual(result_pop_matrix, )
+        self.assertEqual(result_pop_idx, )
